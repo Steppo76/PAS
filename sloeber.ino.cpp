90074a2befd43b5ea8125689446289d4f4b50346
Steppo76 <41750666+Steppo76@users.noreply.github.com>
2018-10-11 21:12:53 +0200

    Initial commit

---
+++ sloeber.ino.cpp
@@ -0,0 +1,17 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-10-11 21:09:56
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "TFT.h"
+
+void setup() ;
+void loop() ;
+
+#include "PAS.ino"
+
+
+#endif
